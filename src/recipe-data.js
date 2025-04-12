faf24e1d9e6d429f919c5f46df9a385860aca892
Misplaced451 <karvemihir@gmail.com>
2025-04-11 23:14:59 -0700

    Add files via upload

---
+++ src/recipe-data.js
@@ -0,0 +1,207 @@
+const RecipeData = {
+    recipes: [
+        {
+            id:1,
+            name:"Apple Filling",
+            path:"/applefilling",
+            tags:['original', 'breakfast', 'dessert', 'beginner', 'vegetarian']
+        },
+
+        {
+            id:2,
+            name:"Cane's Sauce",
+            path:"/canes",
+            tags:['beginner', 'vegetarian']
+        },
+
+        {
+            id:3,
+            name:"Chana Masala",
+            path:"/chana",
+            tags:['indian', 'vegetarian', 'vegan']
+        },
+
+        {
+            id:4,
+            name:"Chicken & Sausage Gumbo",
+            path:"/gumbo",
+            tags:['favorite']
+        },
+
+        {
+            id:5,
+            name:"Cold Brew Coffee",
+            path:"/coffee",
+            tags:['breakfast', 'beginner', 'vegetarian', 'vegan']
+        },
+
+        {
+            id:6,
+            name:"Cranberry Sauce",
+            path:"/cranberry",
+            tags:['original', 'thanksgiving', 'beginner', 'vegetarian', 'vegan']
+        },
+
+        {
+            id:7,
+            name:"Crepes Suzette",
+            path:"/suzette",
+            tags:['fancy', 'dessert', 'vegetarian']
+        },
+
+        {
+            id:8,
+            name:"Cursed Sandwich",
+            path:"/cursed",
+            tags:['original', 'beginner', 'vegetarian', 'vegan']
+        },
+
+        {
+            id:9,
+            name:"Deviled Eggs",
+            path:"/deviledeggs",
+            tags:['fancy', 'beginner', 'vegetarian']
+        },
+
+        {
+            id:10,
+            name:"French Toast",
+            path:"/frenchtoast",
+            tags:['original', 'fancy', 'favorite', 'breakfast', 'vegetarian']
+        },
+
+        {
+            id:11,
+            name:"Fried Egg Sandwich",
+            path:"/eggsandwich",
+            tags:['original', 'weeknight', 'vegetarian']
+        },
+
+        {
+            id:12,
+            name:"Gajar Halwa",
+            path:"/gajarhalwa",
+            tags:['indian', 'dessert', 'vegetarian']
+        },
+
+        {
+            id:13,
+            name:"Gnocchi",
+            path:"/gnocchi",
+            tags:['italian', 'favorite', 'vegetarian']
+        },
+
+        {
+            id:14,
+            name:"Green Chutney",
+            path:"/chutney",
+            tags:['indian', 'beginner', 'vegetarian', 'vegan']
+        },
+
+        {
+            id:15,
+            name:"Lemon Meringue Pie",
+            path:"/lemonmeringue",
+            tags:['original', 'fancy', 'dessert', 'favorite', 'vegetarian']
+        },
+
+        {
+            id:16,
+            name:"Meatballs",
+            path:"/meatballs",
+            tags:['italian', 'beginner', 'weeknight']
+        },
+
+        {
+            id:17,
+            name:"Noodle Soup",
+            path:"/noodlesoup",
+            tags:['original', 'vegetarian']
+        },
+
+        {
+            id:18,
+            name:"Orange Zabaglione",
+            path:"/zabaglione",
+            tags:['italian', 'fancy', 'dessert', 'vegetarian']
+        },
+
+        {
+            id:19,
+            name:"Pancakes",
+            path:"/pancakes",
+            tags:['original', 'breakfast', 'favorite', 'vegetarian']
+        },
+
+        {
+            id:20,
+            name:"Pineapple Upside-Down Deep-Dish Pizza",
+            path:"/pineapplepizza",
+            tags:['original', 'favorite']
+        },
+
+        {
+            id:21,
+            name:"Pizza",
+            path:"/pizza",
+            tags:['italian']
+        },
+
+        {
+            id:22,
+            name:"Rajma",
+            path:"/rajma",
+            tags:['indian', 'favorite', 'vegetarian', 'vegan']
+        },
+
+        {
+            id:23,
+            name:"Ricotta",
+            path:"/ricotta",
+            tags:['italian', 'vegetarian']
+        },
+
+        {
+            id:24,
+            name:"SausagePeppers&Onions Over Rice",
+            path:"/sporice",
+            tags:['original', 'beginner', 'weeknight']
+        },
+
+        {
+            id:25,
+            name:"Sweet Potato Pie",
+            path:"/potatopie",
+            tags:['dessert', 'thanksgiving', 'vegetarian']
+        },
+
+        {
+            id:26,
+            name:"Thanksgiving Cottage Pie",
+            path:"/thanksgivingpie",
+            tags:['original', 'thanksgiving', 'best']
+        },
+
+        {
+            id:27,
+            name:"Tomato Melt",
+            path:"/tomatomelt",
+            tags:['original', 'beginner', 'weeknight', 'best']
+        },
+
+        {
+            id:28,
+            name:"Tomato Sauce",
+            path:"/tomatosauce",
+            tags:['italian', 'original', 'beginner', 'best', 'vegetarian', 'vegan']
+        },
+
+        {
+            id:29,
+            name:"Turkey Rice",
+            path:"/turkeyrice",
+            tags:['original', 'beginner', 'weeknight']
+        }
+    ]
+}
+export default RecipeData;
